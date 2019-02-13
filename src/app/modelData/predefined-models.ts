4b71126c87057d13eb23bf308ba3a0bf70c7145f
Jakub Dziwura <jakub.dziwura@gmail.com>
2019-02-13 22:25:51 +0100

    Dodanie guzików do obsługi wyboru mapy

---
+++ src/app/modelData/predefined-models.ts
@@ -0,0 +1,3 @@
+import SmartCityModel from '../model/smart-city-model';
+
+export const smartCitytModels: SmartCityModel[] = [];
--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SetupService } from '../services/setup.service';
+import SmartCityModel from '../model/smart-city-model';
+import { smartCitytModels } from '../modelData/predefined-models';
 
 @Component({
   selector: 'app-setup',
@@ -8,9 +10,15 @@ import { SetupService } from '../services/setup.service';
 })
 export class SetupComponent implements OnInit {
 
+  predefinedModels: SmartCityModel[]
+  selectedModel: number
+
   constructor(private setupService: SetupService) { }
 
   ngOnInit() {
+    this.predefinedModels = smartCitytModels;
+    if(this.predefinedModels)
+      this.selectedModel = 0;
   }
 
   saveChanges() {
